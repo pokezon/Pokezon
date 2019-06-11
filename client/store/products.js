d3dedc7a6cc25484b9076319849af425c963091c
Amneet Sandhu <amneet954@gmail.com>
2019-06-11 15:45:46 -0400

    Added Product Store + Exported Reducer to Index.JS Combine Reducer

---
+++ client/store/products.js
@@ -0,0 +1,47 @@
+import axios from 'axios'
+
+// THIS IS INITIAL STATE
+const initialState = {
+  selectedProduct: {},
+  allProducts: []
+}
+
+// ACTION TYPE
+
+const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS'
+const GET_PRODUCT = 'GET_PRODUCT'
+
+// ACTION CREATORS
+
+const gotAllProducts = products => ({type: GET_ALL_PRODUCTS, products})
+const gotProduct = product => ({type: GET_PRODUCT, product})
+
+// THUNKS
+
+export const gettingAllProducts = () => async dispatch => {
+  const response = await axios.get('/api/products/')
+  dispatch(gotAllProducts(response.data))
+}
+
+export const gettingProduct = productId => async dispatch => {
+  const response = await axios.get(`/api/products/${productId}`)
+  dispatch(gotProduct(response.data))
+}
+
+//REDUCERS
+
+export default function(state = initialState, action) {
+  const newState = JSON.parse(JSON.stringify(state))
+  // used to make deep copy without the need for spread operator
+  switch (action.type) {
+    case GET_ALL_PRODUCTS:
+      newState.allProducts = action.products
+      break
+    case GET_PRODUCT:
+      newState.selectedProduct = action.product
+      break
+    default:
+      return state
+  }
+  return newState
+}
