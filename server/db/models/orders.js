f1a3e3b2b8646a8b387b212a8e940d64f770ddba
Amneet Sandhu <amneet954@gmail.com>
2019-06-11 11:48:33 -0400

    Added Orders model and left notes for consideration

---
+++ server/db/models/orders.js
@@ -0,0 +1,36 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Order = db.define('order', {
+  userId: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  productId: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+    validate: {
+      min: 0,
+      max: 99
+    }
+  },
+  completedFlag: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+  },
+  orderId: {
+    type: Sequelize.INTEGER
+  }
+})
+
+module.exports = Order
+
+// Quanity - we need something to check before anything is actually executed if we have enough of the product in the inventory to even execute. Possibly a front end consideration.
+
+//For Order ID - We need completedFlag to be true. Then orderId uniquely randomly generated. Must be unique for each executed transaction??? From transitioning cart to purchase
