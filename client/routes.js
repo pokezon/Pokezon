24fe511f7155ca73d5321a9ec0605a53de7461bf
Amneet Sandhu <amneet954@gmail.com>
2019-06-12 12:37:35 -0400

    (new feat) Single Product route

--- client/routes.js
+++ client/routes.js
@@ -2,9 +2,15 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, AllProducts} from './components'
+import {
+  Login,
+  Signup,
+  UserHome,
+  AllProducts,
+  Cart,
+  SingleProduct
+} from './components'
 import {me} from './store'
-
 /**
  * COMPONENT
  */
@@ -19,18 +25,11 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route
-          exact
-          path="/"
-          render={() => (
-            <div>
-              <h1>hello</h1>
-            </div>
-          )}
-        />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/products" component={AllProducts} />
+        <Route exact path="/" component={AllProducts} />
+        <Route exact path="/:id" component={SingleProduct} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/cart" component={Cart} />
 
         {isLoggedIn && (
           <Switch>
