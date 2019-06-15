8ee69707392e9d2bc9fd8adbc1540d917ca0dc0a
Ricardo Pineda <richay718@gmail.com>
2019-06-15 13:46:54 -0400

    beginning to fix quantity in cart merges for multiple of the same items being added to the cart, still buggy

--- client/store/cart.js
+++ client/store/cart.js
@@ -42,7 +42,7 @@ export const gettingCart = () => async dispatch => {
 
 export const addingCartItem = product => async dispatch => {
   try {
-    const response = await axios.post(`/api/cart`, product)
+    const response = await axios.post(`/api/cart`, {...product, quantity: 1})
     const item = await axios.get(`/api/cart/${response.data.id}`)
     dispatch(addCartItem(item.data))
   } catch (error) {
