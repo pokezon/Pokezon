c462ddf9837e5720ede70f7448d41eb9035073ae
dwyfrequency <jdwy215@gmail.com>
2019-06-17 16:43:23 -0400

    (store/cart.js & components/order-history.js) added redux and boilerplate for history comp:

--- client/components/order-history.js
+++ client/components/order-history.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {gettingOrderHistory} from '../store/cart'
 
 export class OrderHistory extends Component {
-  componentDidMount() {}
+  componentDidMount() {
+    this.props.gettingOrderHistory()
+  }
 
   render() {
     return <div>OrderHistory</div>
   }
 }
 
-export default OrderHistory
+const mapStateToProps = state => ({
+  orderHistory: state.orders.orderHistory
+})
+
+const mapDispatchToProps = dispatch => ({
+  gettingOrderHistory: () => {
+    dispatch(gettingOrderHistory())
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(OrderHistory)
