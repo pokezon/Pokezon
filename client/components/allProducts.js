d329a9ab7da51b4a8589ae29b637125e7557b1c7
Arnald Wong <arny1031@gmail.com>
2019-06-18 12:35:50 -0400

    we can now add item from all products page

--- client/components/allProducts.js
+++ client/components/allProducts.js
@@ -2,11 +2,44 @@ import React, {Component} from 'react'
 import {gettingAllProducts} from '../store/products'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
+import {
+  addingCartItem,
+  gettingCart,
+  updatingCartItem,
+  gettingProduct
+} from '../store/cart'
 
 class AllProducts extends Component {
   componentDidMount() {
     this.props.getProducts()
+    if (this.props.isLoggedIn) {
+      this.props.gettingCart()
+    }
   }
+
+  addItem = product => {
+    if (this.props.isLoggedIn) {
+      const foundItemInCart = this.props.cart.find(
+        cartItem => cartItem.productId === product.id
+      )
+      if (foundItemInCart) {
+        this.props.updatingCartItem({
+          id: foundItemInCart.id,
+          quantity: foundItemInCart.quantity + 1
+        })
+      } else {
+        this.props.addCartItem(product)
+      }
+      // that is dispatch
+    } else {
+      let localStorageCart = JSON.parse(
+        localStorage.getItem('LocalStorageCart')
+      )
+      localStorageCart.push({id: localStorageCart.length, product: product})
+      localStorage.setItem('LocalStorageCart', JSON.stringify(localStorageCart))
+    }
+  }
+
   render() {
     return (
       <div>
@@ -20,11 +53,11 @@ class AllProducts extends Component {
               <div className="row">
                 {this.props.allProducts.map(product => {
                   return (
-                    <div key={product.id} className="eachProduct test">
+                    <div key={product.id} className="eachProduct">
                       <br />
                       <div className="thumbnail">
-                        <Link to={`/products/${product.id}`} align="center">
-                          <img src={product.imageUrl} width="75%" />
+                        <Link to={`/products/${product.id}`}>
+                          <img src={product.imageUrl} width="150" />
                         </Link>
                         <br />
                         <div className="thumbnailInfo">
@@ -33,17 +66,19 @@ class AllProducts extends Component {
                           <h5 align="center">
                             <img
                               src="https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-1/135/_Coin-512.png"
-                              width="10%"
-                              align="center"
+                              width="30"
                             />${product.price}
                           </h5>
                           {/* <img src='https://static.thenounproject.com/png/551641-200.png' width='50'/> */}
                           <div align="center">
                             <img
                               src="https://i.ya-webdesign.com/images/open-pokeball-png-8.png"
-                              width="10%"
+                              width="40"
                             />
-                            <button className="btn btn-primary" id="brand-name">
+                            <button
+                              className="btn btn-primary"
+                              onClick={() => this.addItem(product)}
+                            >
                               Add to Cart
                             </button>
                           </div>
@@ -61,12 +96,21 @@ class AllProducts extends Component {
   }
 }
 
-const mapStateToProps = state => ({allProducts: state.products.allProducts})
+const mapStateToProps = state => ({
+  allProducts: state.products.allProducts,
+  product: state.products.selectedProduct,
+  isLoggedIn: !!state.user.id,
+  cart: state.cart.cartItems
+})
 
 // FOR ME(AMNEET): PRODUCTS LAYER IS CREATED FROM REDUCER SO U NEED TO GO TO THAT LAYER
 
 const mapDispatchToProps = dispatch => ({
-  getProducts: () => dispatch(gettingAllProducts())
+  getProducts: () => dispatch(gettingAllProducts()),
+  gettingCart: () => dispatch(gettingCart()),
+  updatingCartItem: item => dispatch(updatingCartItem(item)),
+  etProduct: id => dispatch(gettingProduct(id)),
+  addCartItem: product => dispatch(addingCartItem(product))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(AllProducts)
