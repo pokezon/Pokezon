aacdcf3140a2fd309685995142304bc4d506ac67
dwyfrequency <jdwy215@gmail.com>
2019-06-18 12:50:44 -0400

    (feature/order-item.js) added initial implementation

--- client/components/history/order-item.js
+++ client/components/history/order-item.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import SingleProduct from '../singleProduct'
 
 const OrderItem = ({order}) => {
-  return <div>OrderItem</div>
+  // return <SingleProduct />
+  return (
+    <>
+      <p>
+        Order Id:
+        <span>{order.completedOrderId}</span>
+      </p>
+      <p>
+        Product Name: <span>{order.product.name}</span>
+      </p>
+      <p>
+        Created At:<span>{order.createdAt}</span>
+      </p>
+    </>
+  )
 }
 
 export default OrderItem
