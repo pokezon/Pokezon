93132c73aa0cc799eb0074328fdc098fffd55e87
dwyfrequency <jdwy215@gmail.com>
2019-06-18 12:36:12 -0400

    updated api route to sort createAt and compeltedOrderId

---
+++ client/components/history/order-item.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const OrderItem = ({order}) => {
+  return <div>OrderItem</div>
+}
+
+export default OrderItem
--- client/components/order-history.js
+++ client/components/order-history.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {gettingOrderHistory} from '../store/cart'
+import OrderItem from './history/order-item'
 
 export class OrderHistory extends Component {
   componentDidMount() {
@@ -10,7 +11,16 @@ export class OrderHistory extends Component {
   render() {
     const {orderHistory} = this.props
     console.log(orderHistory)
-    return orderHistory.length && <div>OrderHistory</div>
+    return orderHistory.length ? (
+      <>
+        <div>OrderHistory</div>
+        {orderHistory.map((order, idx) => (
+          <OrderItem key={idx} order={order} />
+        ))}
+      </>
+    ) : (
+      <h1>No previous order</h1>
+    )
   }
 }
 
